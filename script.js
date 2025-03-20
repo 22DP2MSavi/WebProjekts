3447c4cb8a1d29a3de108361b4419f883dc2e040
22DP2MSavi <144315280+22DP2MSavi@users.noreply.github.com>
2025-03-20 19:11:46 +0200

    v2

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
         document.body.style.backgroundColor = '#333';
         document.body.style.color = '#fff';
     } else {
-        document.body.style.backgroundColor = '#fff8dc';
+        document.body.style.backgroundColor = '#87CEEB';
         document.body.style.color = '#333';
     }
 });
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => tab.style.display = 'none');
+    document.getElementById(tabId).style.display = 'block';
+}
